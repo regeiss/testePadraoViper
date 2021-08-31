1416aa120981f4098d3bb628e0aef15633608fa6
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2021-08-31 17:35:35 -0300

    Initial Commit

---
+++ testePadraoViper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testePadraoViper
+//
+//  Created by Roberto Edgar Geiss on 31/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testePadraoViper/testePadraoViperApp.swift
@@ -0,0 +1,17 @@
+//
+//  testePadraoViperApp.swift
+//  testePadraoViper
+//
+//  Created by Roberto Edgar Geiss on 31/08/21.
+//
+
+import SwiftUI
+
+@main
+struct testePadraoViperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
